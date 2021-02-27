ce3da35377d9a457d4d8726449313db994d1142b
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-02-27 15:19:56 +0000

    MakeXMLs now references accountdb properly

--- makexmls.py
+++ makexmls.py
@@ -1,4 +1,4 @@
-import json,os
+import glob,json,os
 from charparser import makexml
 
 accountdb = "data/accountdb.json"
@@ -7,14 +7,17 @@ accounts = {}
 if os.path.exists(accountdb):
     with open(accountdb) as json_file:
         accounts = json.load(json_file)
-        for account in accounts:
-            for char in accounts[account]:    
-                datafile = f'data/{account}-{char}.json'
-                if os.path.exists(datafile):
-                    with open(f'data/{account}-{char}.json', encoding='utf-8') as json_file:
-                        chardata = json.load(json_file)
-                        print(f'{account} - {char}')
+        POEChars = glob.glob('data/*-*.json')
+        for POEChar in POEChars:
+            account,char = os.path.basename(POEChar).replace(".json","").split("-")
+            if os.path.exists(POEChar):
+                with open(POEChar, encoding='utf-8') as json_file:
+                    chardata = json.load(json_file)
+                    print(f'{account} - {char}')
+                    if char in accounts[account]:
                         root = makexml(account,char,chardata,accounts[account][char])
+                    else:
+                        print("JSON without entry in accountdb " + account + " " + char)
 with open(accountdb, 'w') as json_file:
     json.dump(accounts, json_file, indent=4)
 
