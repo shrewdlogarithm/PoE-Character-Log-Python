1b975d70e069597ac2cce769dcf0c0d489eeedda
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-02-28 23:37:05 +0000

    Integrated makelog and makexml into rebuildlogxml - in line with PS version

---
+++ rebuildlogxml.py
@@ -0,0 +1,37 @@
+import glob,json,os
+from charparser import makelogs,makexml
+
+accountdb = "data/accountdb.json"
+accounts = {}
+
+if os.path.exists(accountdb):
+    with open(accountdb) as json_file:
+        accounts = json.load(json_file)
+
+POEChars = glob.glob('data/*-*.json')
+
+for POEChar in POEChars:
+
+    account,char = os.path.basename(POEChar).replace(".json","").split("-")
+
+    logs = glob.glob(f'logs/{account}-{char}.*')
+    for log in logs:
+        os.remove(log)
+
+    with open(POEChar, encoding='utf-8') as json_file:
+        chardata = json.load(json_file)
+        print(f'{account} - {char}')
+        updated = False
+        for i in range(1,len(chardata)):
+            if makelogs(account,char, chardata[i-1], chardata[i]):
+                updated=True
+        if updated:
+            print("Updated")
+        if char in accounts[account]:
+            root = makexml(account,char,chardata,accounts[account][char])
+        else:
+            root = makexml(account,char,chardata,{})
+
+with open(accountdb, 'w') as json_file:
+    json.dump(accounts, json_file, indent=4)
+
