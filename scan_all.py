e8e7c2b12c255f4a396d204679f0c7d87fae9377
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-03-13 18:55:46 +0000

    Scan chars within accounts - easier on the API

--- scan_all.py
+++ scan_all.py
@@ -39,12 +39,14 @@ while 1==1:
         with open("settings.json", 'w') as json_file:
             json.dump(settings, json_file, indent=4)
 
-    chars = []
     if os.path.exists(accountdb):
         with open(accountdb) as json_file:
             accounts = json.load(json_file)
 
     for account in settings["toscan"]:
+
+        toscan = []
+
         try:
             tolog(f"Scanning Account {account}")
             if account not in accounts:
@@ -59,7 +61,7 @@ while 1==1:
                         accounts[account][archchar] = accounts[account][apichar["name"]]
                         accounts[account][archchar]["name"] = archchar
                         accounts[account][apichar["name"]] = {}
-                        chars.append({
+                        toscan.append({
                             "account": account,
                             "char": apichar["name"]
                         })
@@ -67,16 +69,23 @@ while 1==1:
                         if "experience" in accounts[account][apichar["name"]] and accounts[account][apichar["name"]]["experience"] != apichar["experience"]:
                             if os.path.exists(f'data/{account}-{apichar["name"]}.json'):
                                 tolog (f'{apichar["name"]} ({apichar["level"]}) has been active')
-                                chars.append({
+                                toscan.append({
                                     "account": account,
                                     "char": apichar["name"]
                                 })
                             else:
                                 tolog (f'{apichar["name"]} ({apichar["level"]}) has been active but no history - ignoring')
                 else:
-                    if int(apichar["level"]) < int(settings["minlevel"]):
+                    dbname = f'data/{account}-{apichar["name"]}.json'
+                    if os.path.exists(dbname):
+                        tolog (f'{apichar["name"]} ({apichar["level"]}) recreating in accountdb!')
+                        toscan.append({
+                            "account": account,
+                            "char": apichar["name"]
+                        })
+                    elif int(apichar["level"]) < int(settings["minlevel"]):
                         tolog (f'{apichar["name"]} ({apichar["level"]}) is new!')
-                        chars.append({
+                        toscan.append({
                             "account": account,
                             "char": apichar["name"]
                         })
@@ -86,55 +95,53 @@ while 1==1:
                     accounts[account][apichar["name"]] = {}
                 for val in apichar:
                     accounts[account][apichar["name"]][val] = apichar[val]
+            for char in toscan:
+                try:
+                    tolog(f'Scanning Char {char["account"]} - {char["char"]}')
+                    scantime = datetime.now()
+                    dbname = f'data/{char["account"]}-{char["char"]}.json'
+                    passives = session.get(f'https://api.pathofexile.com/character-window/get-passive-skills?reqData=0&accountName={char["account"]}&realm=pc&character={char["char"]}')
+                    passivedb = passives.json()
+                    payload = {'accountName':char["account"], 'character': char["char"]}
+                    items = session.get(url = 'https://api.pathofexile.com/character-window/get-items' , params = payload)
+                    itemdb = items.json()
+                    chardata = [{
+                            "update": scantime,
+                            "character": itemdb["character"],
+                            "items": [],
+                            "passives": []
+                    }]
+                    if os.path.exists(dbname):
+                        with open(dbname) as json_file:
+                            chardata = json.load(json_file)
+                    chardata.append({
+                            "update": scantime,
+                            "character": itemdb["character"],
+                            "items": itemdb["items"],
+                            "passives": passivedb["hashes"]
+                    })
+
+                    if len(chardata) > 1:
+                        tolog(makelogs(char['account'],char['char'],chardata[len(chardata)-2], chardata[len(chardata)-1]))
+                        makexml(char['account'],char['char'],chardata,accounts[char["account"]][char["char"]])
+
+                    with open(dbname, 'w') as json_file:
+                        json.dump(chardata, json_file, indent=4, default=str)
+
+                except:
+                    tolog("Error during Character Scan")
+                    track = traceback.format_exc()
+                    tolog(track)
+                    mywait(settings["longsleep"])
+
         except:
             tolog("Error during Account Scan")
             track = traceback.format_exc()
             tolog(track)
             mywait(settings["longsleep"])
 
-        mywait(settings["shortsleep"])
-
-    for char in chars:
-        try:
-            tolog(f'Scanning Char {char["account"]} - {char["char"]}')
-            scantime = datetime.now()
-            dbname = f'data/{char["account"]}-{char["char"]}.json'
-            passives = session.get(f'https://api.pathofexile.com/character-window/get-passive-skills?reqData=0&accountName={char["account"]}&realm=pc&character={char["char"]}')
-            passivedb = passives.json()
-            payload = {'accountName':char["account"], 'character': char["char"]}
-            items = session.get(url = 'https://api.pathofexile.com/character-window/get-items' , params = payload)
-            itemdb = items.json()
-            chardata = [{
-                    "update": scantime,
-                    "character": itemdb["character"],
-                    "items": [],
-                    "passives": []
-            }]
-            if os.path.exists(dbname):
-                with open(dbname) as json_file:
-                    chardata = json.load(json_file)
-            chardata.append({
-                    "update": scantime,
-                    "character": itemdb["character"],
-                    "items": itemdb["items"],
-                    "passives": passivedb["hashes"]
-            })
-
-            if len(chardata) > 1:
-                tolog(makelogs(char['account'],char['char'],chardata[len(chardata)-2], chardata[len(chardata)-1]))
-                makexml(char['account'],char['char'],chardata,accounts[char["account"]][char["char"]])
-
-            with open(dbname, 'w') as json_file:
-                json.dump(chardata, json_file, indent=4, default=str)
-
-            with open(accountdb, 'w') as json_file:
-                json.dump(accounts, json_file, indent=4)
-
-        except:
-            tolog("Error during Character Scan")
-            track = traceback.format_exc()
-            tolog(track)
-            mywait(settings["longsleep"])
+        with open(accountdb, 'w') as json_file:
+            json.dump(accounts, json_file, indent=4)
 
         mywait(settings["shortsleep"])
 
