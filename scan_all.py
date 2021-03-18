0f8600361dd27277985baca01f543dd09aca2932
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-03-18 10:52:55 +0000

    Added some error handling

--- scan_all.py
+++ scan_all.py
@@ -33,16 +33,23 @@ def archivedata(account,char):
 
 while 1==1:
 
-    if os.path.exists("settings.json"):
-        with open("settings.json") as json_file:
-            settings = json.load(json_file)
-    else:
-        with open("settings.json", 'w') as json_file:
-            json.dump(settings, json_file, indent=4)
+    try: 
+        if os.path.exists("settings.json"):
+            with open("settings.json") as json_file:
+                settings = json.load(json_file)
+        else:
+            with open("settings.json", 'w') as json_file:
+                json.dump(settings, json_file, indent=4)
+    except:
+        tolog("Error: settings file invalid or corrupted")
 
-    if os.path.exists(accountdb):
-        with open(accountdb) as json_file:
-            accounts = json.load(json_file)
+    try:
+        if os.path.exists(accountdb):
+            with open(accountdb) as json_file:
+                accounts = json.load(json_file)
+    except:
+        tolog(f'Error: accountdb file invalid or corrupted')
+
 
     for account in accounts:
         for char in accounts[account]:
@@ -61,49 +68,52 @@ while 1==1:
             if account not in accounts:
                 accounts[account] = {}
             apichars = session.get(f"https://api.pathofexile.com/character-window/get-characters?accountName={account}&realm=pc")
-            apichardb = apichars.json()
-            for apichar in apichardb:
-                if apichar["name"] in accounts[account]:
-                    if "level" in accounts[account][apichar["name"]] and int(accounts[account][apichar["name"]]["level"]) > int(apichar["level"]):
-                        tolog (f'{apichar["name"]} has been rerolled - archiving old character data')
-                        archchar = archivedata(account,apichar["name"])
-                        accounts[account][archchar] = accounts[account][apichar["name"]]
-                        accounts[account][archchar]["name"] = archchar
-                        accounts[account][apichar["name"]] = {}
-                        toscan.append({
-                            "account": account,
-                            "char": apichar["name"]
-                        })
-                    elif int(apichar["level"]) < int(settings["maxlevel"]):
-                        if "experience" in accounts[account][apichar["name"]] and accounts[account][apichar["name"]]["experience"] != apichar["experience"]:
-                            if os.path.exists(f'data/{account}-{apichar["name"]}.json'):
-                                tolog (f'{apichar["name"]} ({apichar["level"]}) has been active')
-                                toscan.append({
-                                    "account": account,
-                                    "char": apichar["name"]
-                                })
-                            else:
-                                tolog (f'{apichar["name"]} ({apichar["level"]}) has been active but no history - ignoring')
-                else:
-                    dbname = f'data/{account}-{apichar["name"]}.json'
-                    if os.path.exists(dbname):
-                        tolog (f'{apichar["name"]} ({apichar["level"]}) recreating in accountdb!')
-                        toscan.append({
-                            "account": account,
-                            "char": apichar["name"]
-                        })
-                    elif int(apichar["level"]) < int(settings["minlevel"]):
-                        tolog (f'{apichar["name"]} ({apichar["level"]}) is new!')
-                        toscan.append({
-                            "account": account,
-                            "char": apichar["name"]
-                        })
+            if apichars.status_code != 200:
+                tolog(f'Error: failed to read characters for account {account}')
+            else:
+                apichardb = apichars.json()
+                for apichar in apichardb:
+                    if apichar["name"] in accounts[account]:
+                        if "level" in accounts[account][apichar["name"]] and int(accounts[account][apichar["name"]]["level"]) > int(apichar["level"]):
+                            tolog (f'{apichar["name"]} has been rerolled - archiving old character data')
+                            archchar = archivedata(account,apichar["name"])
+                            accounts[account][archchar] = accounts[account][apichar["name"]]
+                            accounts[account][archchar]["name"] = archchar
+                            accounts[account][apichar["name"]] = {}
+                            toscan.append({
+                                "account": account,
+                                "char": apichar["name"]
+                            })
+                        elif int(apichar["level"]) < int(settings["maxlevel"]):
+                            if "experience" in accounts[account][apichar["name"]] and accounts[account][apichar["name"]]["experience"] != apichar["experience"]:
+                                if os.path.exists(f'data/{account}-{apichar["name"]}.json'):
+                                    tolog (f'{apichar["name"]} ({apichar["level"]}) has been active')
+                                    toscan.append({
+                                        "account": account,
+                                        "char": apichar["name"]
+                                    })
+                                else:
+                                    tolog (f'{apichar["name"]} ({apichar["level"]}) has been active but no history - ignoring')
                     else:
-                        tolog (f'{apichar["name"]} ({apichar["level"]}) is new but over Level {settings["minlevel"]} - ignoring')
-                if apichar["name"] not in accounts[account]:
-                    accounts[account][apichar["name"]] = {}
-                for val in apichar:
-                    accounts[account][apichar["name"]][val] = apichar[val]
+                        dbname = f'data/{account}-{apichar["name"]}.json'
+                        if os.path.exists(dbname):
+                            tolog (f'{apichar["name"]} ({apichar["level"]}) recreating in accountdb!')
+                            toscan.append({
+                                "account": account,
+                                "char": apichar["name"]
+                            })
+                        elif int(apichar["level"]) < int(settings["minlevel"]):
+                            tolog (f'{apichar["name"]} ({apichar["level"]}) is new!')
+                            toscan.append({
+                                "account": account,
+                                "char": apichar["name"]
+                            })
+                        else:
+                            tolog (f'{apichar["name"]} ({apichar["level"]}) is new but over Level {settings["minlevel"]} - ignoring')
+                    if apichar["name"] not in accounts[account]:
+                        accounts[account][apichar["name"]] = {}
+                    for val in apichar:
+                        accounts[account][apichar["name"]][val] = apichar[val]
             
             for char in toscan:
             
